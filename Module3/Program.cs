4f4a96a48390b43354c6718ffb2de26e8ab643e3
SCNomeD <vladiiimir1991@gmail.com>
2022-02-06 20:16:19 +0300

    Арифметические операторы Unit3_6

--- Module3/Program.cs
+++ Module3/Program.cs
@@ -6,48 +6,54 @@ namespace Module3
     {
         static void Main(string[] args)
         {
-            //    const string MyName = "Владимир";
-            //    const byte MyAge = 30;
-            //    const bool Pet = false;
-            //    const double Size = 42.5;
+        //    const string MyName = "Владимир";
+        //    const byte MyAge = 30;
+        //    const bool Pet = false;
+        //    const double Size = 42.5;
+        //    Console.WriteLine("Меня зовут {0}", MyName);
+        //    Console.WriteLine("Мне {0} лет", MyAge);
+        //    Console.WriteLine("У вас есть домашнее животное? {0}", Pet);
+        //    Console.WriteLine("Мой размер обуви {0}", Size);
+        //    Console.WriteLine("IntMin {0} ", int.MinValue);
+        //    Console.WriteLine("IntMax {0} ", int.MaxValue);
+        //    Console.WriteLine("DoubleMin {0} ", double.MinValue);
+        //    Console.WriteLine("DoubleMax {0} ", double.MaxValue);
+        //    Console.ReadKey();
+        //}
+        //enum DaysOfWeek : byte
+        //{
+        //    Monday = 1,
+        //    Tuesday,
+        //    Wednesday,
+        //    Thursday,
+        //    Friday,
+        //    Saturday,
+        //    Sunday
+        //}
+        //enum Semaphore
+        //{
+        //    Red = 100,
+        //    Yellow = 200,
+        //    Green = 300
+        //}
 
-            //    Console.WriteLine("Меня зовут {0}", MyName);
-            //    Console.WriteLine("Мне {0} лет", MyAge);
-            //    Console.WriteLine("У вас есть домашнее животное? {0}", Pet);
-            //    Console.WriteLine("Мой размер обуви {0}", Size);
+        //    string MyName = "Computer";
+        //    Console.WriteLine("Привет, человек");
+        //    Console.WriteLine("Меня зовут {0}", MyName);
+        //    Console.Write("А как тебя зовут? ");
+        //    string YourName = Console.ReadLine();
+        //    Console.Write("Рад встрече, {0} ", YourName);
+        //    Console.ReadKey();
+        //}
 
-            //    Console.WriteLine("IntMin {0} ", int.MinValue);
-            //    Console.WriteLine("IntMax {0} ", int.MaxValue);
-            //    Console.WriteLine("DoubleMin {0} ", double.MinValue);
-            //    Console.WriteLine("DoubleMax {0} ", double.MaxValue);
-
-            //    Console.ReadKey();
-            //}
-            //enum DaysOfWeek : byte
-            //{
-            //    Monday = 1,
-            //    Tuesday,
-            //    Wednesday,
-            //    Thursday,
-            //    Friday,
-            //    Saturday,
-            //    Sunday
-            //}
-            //enum Semaphore
-            //{
-            //    Red = 100,
-            //    Yellow = 200,
-            //    Green = 300
-            //}
-            string MyName = "Computer";
-            Console.WriteLine("Привет, человек");
-            Console.WriteLine("Меня зовут {0}", MyName);
-            Console.Write("А как тебя зовут? ");
-            string YourName = Console.ReadLine();
-            Console.Write("Рад встрече, {0} ", YourName);
-
+        //    double five = 5;
+        //    double two = 2;
+        //    double result = five / two;
+        //    Console.WriteLine("5 / 2 = {0}", result);
+        //    Console.ReadKey();
+            double result = 5.0 / 2.0;
+            Console.WriteLine("5 / 2 = {0}", result);
             Console.ReadKey();
         }
     }
-
 }
