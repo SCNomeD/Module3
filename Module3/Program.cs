05bd96bdf0713ba7cb2d267025ff008b986978b6
SCNomeD <vladiiimir1991@gmail.com>
2022-02-06 16:29:15 +0300

    Добавьте файлы проекта.

---
+++ Module3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Module3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName;
+            MyName = "Владимир";
+
+            Console.WriteLine(MyName);
+
+            Console.ReadKey();
+        }
+    }
+}
