9385fe2729e8dc1716b637bf692fac3b5af1e108
SCNomeD <vladiiimir1991@gmail.com>
2022-02-06 19:51:10 +0300

    Complite unit 3_5

--- Module3/Program.cs
+++ Module3/Program.cs
@@ -6,47 +6,45 @@ namespace Module3
     {
         static void Main(string[] args)
         {
-        //    const string MyName = "Владимир";
-        //    const byte MyAge = 30;
-        //    const bool Pet = false;
-        //    const double Size = 42.5;
+            //    const string MyName = "Владимир";
+            //    const byte MyAge = 30;
+            //    const bool Pet = false;
+            //    const double Size = 42.5;
 
-        //    Console.WriteLine("Меня зовут {0}", MyName);
-        //    Console.WriteLine("Мне {0} лет", MyAge);
-        //    Console.WriteLine("У вас есть домашнее животное? {0}", Pet);
-        //    Console.WriteLine("Мой размер обуви {0}", Size);
+            //    Console.WriteLine("Меня зовут {0}", MyName);
+            //    Console.WriteLine("Мне {0} лет", MyAge);
+            //    Console.WriteLine("У вас есть домашнее животное? {0}", Pet);
+            //    Console.WriteLine("Мой размер обуви {0}", Size);
 
-        //    Console.WriteLine("IntMin {0} ", int.MinValue);
-        //    Console.WriteLine("IntMax {0} ", int.MaxValue);
-        //    Console.WriteLine("DoubleMin {0} ", double.MinValue);
-        //    Console.WriteLine("DoubleMax {0} ", double.MaxValue);
+            //    Console.WriteLine("IntMin {0} ", int.MinValue);
+            //    Console.WriteLine("IntMax {0} ", int.MaxValue);
+            //    Console.WriteLine("DoubleMin {0} ", double.MinValue);
+            //    Console.WriteLine("DoubleMax {0} ", double.MaxValue);
 
-        //    Console.ReadKey();
-        //}
-        //enum DaysOfWeek : byte
-        //{
-        //    Monday = 1,
-        //    Tuesday,
-        //    Wednesday,
-        //    Thursday,
-        //    Friday,
-        //    Saturday,
-        //    Sunday
-        //}
-        //enum Semaphore
-        //{
-        //    Red = 100,
-        //    Yellow = 200,
-        //    Green = 300
-        //}
-            string MyName = "Владимир";
-            string age = "30";
-            Console.WriteLine("Привет, мир");
-            Console.WriteLine();
-            //Console.WriteLine("Меня зовут " + MyName);
-            //Console.WriteLine($"Меня зовут {MyName}");
+            //    Console.ReadKey();
+            //}
+            //enum DaysOfWeek : byte
+            //{
+            //    Monday = 1,
+            //    Tuesday,
+            //    Wednesday,
+            //    Thursday,
+            //    Friday,
+            //    Saturday,
+            //    Sunday
+            //}
+            //enum Semaphore
+            //{
+            //    Red = 100,
+            //    Yellow = 200,
+            //    Green = 300
+            //}
+            string MyName = "Computer";
+            Console.WriteLine("Привет, человек");
             Console.WriteLine("Меня зовут {0}", MyName);
-            Console.WriteLine($"Мой возраст {age}");
+            Console.Write("А как тебя зовут? ");
+            string YourName = Console.ReadLine();
+            Console.Write("Рад встрече, {0} ", YourName);
 
             Console.ReadKey();
         }
