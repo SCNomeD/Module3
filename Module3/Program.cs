2028e85108ba17bdc64e272083579bfbbc99d314
SCNomeD <vladiiimir1991@gmail.com>
2022-02-06 17:52:17 +0300

    Изменения в программе (типы данных)

--- Module3/Program.cs
+++ Module3/Program.cs
@@ -7,16 +7,15 @@ namespace Module3
         static void Main(string[] args)
         {
             const string MyName = "Владимир";
+            const byte MyAge = 30;
+            const bool Pet = false;
+            const double Size = 42.5;
 
-            Console.WriteLine(MyName);
+            Console.WriteLine("Меня зовут {0}", MyName);
+            Console.WriteLine("Мне {0} лет", MyAge);
+            Console.WriteLine("У вас есть домашнее животное? {0}", Pet);
+            Console.WriteLine("Мой размер обуви {0}", Size);
 
-            Console.WriteLine("\tПривет, мир");
-            Console.WriteLine("\tМне 30 лет");
-            Console.WriteLine("\tМое имя \n Владимир");
-            Console.WriteLine("\u0040");
-            Console.WriteLine("\u0023");
-            Console.WriteLine("\x40");
-            Console.WriteLine("\x23");
 
             Console.ReadKey();
         }
